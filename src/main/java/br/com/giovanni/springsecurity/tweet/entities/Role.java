a75c0cd47bf1b6e9a1c2270e8e520bbdee26e50f
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-12-02 22:24:49 -0300

    Created getters and setter the entities and created a enum to roles the user

--- src/main/java/br/com/giovanni/springsecurity/tweet/entities/Role.java
+++ src/main/java/br/com/giovanni/springsecurity/tweet/entities/Role.java
@@ -12,4 +12,36 @@ public class Role {
     private Long roleId;
 
     private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values {
+
+        ADMIN(1L),
+        BASIC(2L);
+
+        long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+        public long getRoleId() {
+            return roleId;
+        }
+    }
 }
--- src/main/java/br/com/giovanni/springsecurity/tweet/entities/Tweet.java
+++ src/main/java/br/com/giovanni/springsecurity/tweet/entities/Tweet.java
@@ -20,4 +20,36 @@ public class Tweet {
 
     @CreationTimestamp
     private Instant creationTimestamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
 }
