51c831b2ca55352f403f144d1dcf882a8148cea8
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-12-02 22:20:55 -0300

    Created column names and made the relations between entities

--- src/main/java/br/com/giovanni/springsecurity/tweet/entities/User.java
+++ src/main/java/br/com/giovanni/springsecurity/tweet/entities/User.java
@@ -11,12 +11,20 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
     private UUID userId;
 
+    @Column(unique = true)
     private String username;
 
     private String password;
 
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)//manter alterações nos dois e pra trazer sempre o relacionamento
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles;
 
 }
