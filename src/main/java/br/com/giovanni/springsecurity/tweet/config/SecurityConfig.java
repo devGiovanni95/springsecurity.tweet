4c5f3f5550959f93724792a8ffd3e2c1995a5aaf
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-12-08 12:48:17 -0300

    Alter method SecurityFilterChain to permit request on /login

--- src/main/java/br/com/giovanni/springsecurity/tweet/config/SecurityConfig.java
+++ src/main/java/br/com/giovanni/springsecurity/tweet/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,7 +35,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll() //liberando a rota de loginus
+
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable()) //fazer somente em devl pra ficar mais facil testar e nunca em prod
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
